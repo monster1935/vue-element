b0af0993ec92b6b56cf9163c7bab2ae23a9fb30a
monster1935 <liyb2014@163.com>
2016-12-15 17:46:40 +0800

    修改主页面显示

--- src/components/common/SideNav.vue
+++ src/components/common/SideNav.vue
@@ -19,14 +19,5 @@
   </el-col>
 
 
-   <el-menu default-active="/testManagement" class="el-menu-vertical-demo" router>
-
-     <el-menu-item index="testManagement"><i class="el-icon-menu"></i>测试管理</el-menu-item>
-     <el-menu-item index="userGroup"><i class="el-icon-setting"></i>用户分群管理</el-menu-item>
-     <hr>
-     <el-menu-item index="help"><i class="el-icon-menu"></i>帮助中心</el-menu-item>
-     <el-menu-item index="feedback"><i class="el-icon-setting"></i>意见反馈</el-menu-item>
-   </el-menu>
-
 
 </template>
